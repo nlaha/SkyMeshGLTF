641f065927b274a7886fe0f601402364a695912f
Nathan Laha <nlaha@outlook.com>
2021-03-05 23:21:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-# Press the green button in the gutter to run the script.
 import os
 from colorama import init
 import subprocess as sp
